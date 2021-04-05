84f00b8c341e592dbde1df690ed9c8bb88498a24
Abdullah Hashim <abdul@hey.com>
2021-04-05 08:15:47 -0400

    Discussion show, with inline editing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: %i[index new create edit update destroy]
+  resources :discussions
   root to: "main#index"
 end
